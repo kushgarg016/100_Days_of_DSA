2f34fcd390c81943d8dc284a5ae4422f82e7fcc2
kushgarg016 <kushgarg016@gmail.com>
2026-02-04 23:37:05 +0530

    Add files via upload

---
+++ Day2_Q4.c
@@ -0,0 +1,14 @@
+int maxProfit(int* prices, int pricesSize) {
+    int buyPrice = prices[0];
+    int profit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < buyPrice) {
+            buyPrice = prices[i];
+        } else if (prices[i] - buyPrice > profit) {
+            profit = prices[i] - buyPrice;
+        }
+    }
+
+    return profit;
+}
